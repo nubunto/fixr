6458353cdd2bfd04057cebf3c1b14e99ac2d6d94
Bruno Panuto <bl.panuto@gmail.com>
2015-08-25 23:39:47 -0300

    added package radix and started designing data

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"time"
 	"github.com/tucnak/telebot"
+	"github.com/mediocregopher/radix.v2/redis"
 	"fmt"
 	"flag"
 )
@@ -21,28 +22,52 @@ func (c command) Send(message telebot.Message, bot *telebot.Bot) {
 func NewCommand(message string, keyboard [][]string) *command {
 	return &command {
 		Message: message,
-		Markup: telebot.ReplyMarkup{ CustomKeyboard: keyboard },
+		Markup: telebot.ReplyMarkup{ OneTimeKeyboard: true, CustomKeyboard: keyboard, ResizeKeyboard: true, },
 	}
 }
 
-var token = flag.String("token", "The bot token", "")
+
 
 func main() {
 	flag.Parse()
+	var token = flag.String("token", "The bot token", "")
 	bot, err := telebot.NewBot(*token)
 	if err != nil {
-		fmt.Printf("Error: %v", err)
-		return
+		fmt.Errorf("somethings wrong, goodbye. %v", err)
 	}
-
+	redis_cli, err := redis.Dial("tcp", "localhost:6379")
+	if err != nil {
+		fmt.Errorf("redis_cli died.")
+	}
 	messages := make(chan telebot.Message)
 	bot.Listen(messages, 1 * time.Second)
-
 	for message := range messages {
-		if message.Text == "/hi" {
-			c := NewCommand("Hello, " + message.Sender.FirstName, [][]string{ { "fuck", "you" } })
-			c.Send(message, bot)
+		if message.Text == "/start" {
+			handleSubscription(message, bot, redis_cli)
 		}
 	}
 }
 
+func handleSubscription(message telebot.Message, bot *telebot.Bot, redis_cli *redis.Client) {
+	isSubscribed, err := redis_cli.Cmd("SISMEMBER", "registered_users", message.Sender.ID).Int()
+	if err != nil {
+		fmt.Errorf("somethings wrong, goodbye. %v", err)
+	}
+	if isSubscribed == 1 {
+		NewCommand("You are already subscribed", nil).Send(message, bot)
+	} else {
+		next_conn, err := redis_cli.Cmd("INCR", "user_id").Int()
+		if err != nil {
+			fmt.Errorf("somethings wrong, goodbye. %v", err)
+		}
+		err = redis_cli.Cmd("SADD", "registered_users", message.Sender.ID).Err
+		if err != nil {
+			fmt.Errorf("somethings wrong, goodbye. %v", err)
+		}
+		err = redis_cli.Cmd("HMSET", fmt.Sprintf("users:%d", next_conn), "username", message.Sender.Username, "id", message.Sender.ID).Err
+		if err != nil {
+			fmt.Errorf("somethings wrong, goodbye. %v", err)
+		}
+		NewCommand("Welcome aboard, " + message.Sender.FirstName + "! We're happy to have you here.", nil).Send(message, bot)
+	}
+}

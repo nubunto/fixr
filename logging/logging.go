bb7fdda2e5b22c5de53f43ea53912d647dcfd1d5
Bruno Panuto <bl.panuto@gmail.com>
2015-09-15 23:59:16 -0300

    logging moved to another package so other packages can use this

---
+++ logging/logging.go
@@ -0,0 +1,25 @@
+package logging
+
+import (
+	"log"
+	"io"
+)
+
+var (
+	infoLogger *log.Logger
+	errorLogger *log.Logger
+)
+
+func Start(writeTo io.Writer) {
+	logType := log.Ldate | log.Ltime | log.Lshortfile
+	infoLogger = log.New(writeTo, "INFO: ", logType)
+	errorLogger = log.New(writeTo, "ERROR :", logType)
+}
+
+func Error(err error) {
+	errorLogger.Println(err)
+}
+
+func Info(msg string) {
+	infoLogger.Println(msg)
+}
